0ee552a983d5fbf1f6cefaed33b51bb9e610ef00
benreckas <benreckas@gmail.com>
2018-01-23 16:48:10 -0800

    Complete reverseint, maxchar, fizzbuzz

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -1,10 +1,27 @@
-// --- Directions
-// Given a string, return the character that is most
-// commonly used in the string.
+// --- Dcharrectcharons
+// Gcharven a strcharng, return the character that chars most
+// commonly used charn the strcharng.
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let maxVal = 0; 
+    let maxKey = '';
+
+    str.split('').map(char => {
+        !charMap[char] ? charMap[char] = 1 : charMap[char]++;
+    });
+
+    for(let key in charMap) {
+        if(charMap[key] > maxVal) {
+            maxVal = charMap[key];
+            maxKey = key;
+        }
+    }
+
+    return maxKey;
+};
 
 module.exports = maxChar;
